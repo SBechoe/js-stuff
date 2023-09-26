56e4ae764feb791d7b486f9570ca43ff5a135a95
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2023-09-26 12:35:04 +0200

    Basis cijfers berekenen af

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//Boxes
+const totalBox = document.querySelector('.total-box');
+const pointsAmountBox = document.querySelector('.points-amount');
+const resultBox  = document.querySelector('.result-box');
+
+const totalPoints = document.querySelector('.total-points');
+const confirmPointsBtn = document.querySelector('.confirm');
+const pointsAmount = document.querySelector('.amount');
+const calcGradeBtn = document.querySelector('.calc-grade');
+const gradeEl = document.querySelector('.grade');
+
+let grade = 0;
+
+confirmPointsBtn.addEventListener('click', confirmPoints);
+calcGradeBtn.addEventListener('click', calculateGrade)
+
+function confirmPoints() {
+    pointsAmountBox.classList.toggle('visible-box');
+    totalBox.classList.toggle('visible-box');
+}
+
+function calculateGrade() {    
+    resultBox.classList.toggle('visible-box');
+    grade = (pointsAmount.value / totalPoints.value) * 9 + 1;
+
+    gradeEl.textContent = grade;
+}
