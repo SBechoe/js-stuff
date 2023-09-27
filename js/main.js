d22a283a8e4e6510f0e366fd9e6814359fc36e06
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2023-09-27 11:28:11 +0200

    added toFixed  to grade

--- js/main.js
+++ js/main.js
@@ -17,15 +17,15 @@ calcGradeBtn.addEventListener('click', calculateGrade)
 function confirmPoints() {
     if (!totalPoints.value == "") {
         pointsAmountBox.classList.toggle('visible-box');
+        resultBox.classList.toggle('visible-box');
         totalBox.classList.toggle('visible-box');
     }
 }
 
 function calculateGrade() {
-    if (!pointsAmount.value == "") {
-        resultBox.classList.toggle('visible-box');
+    if (!pointsAmount.value == "" && pointsAmount.value <= totalPoints.value) {
         grade = (pointsAmount.value / totalPoints.value) * 9 + 1;
-
-        gradeEl.textContent = grade;
+        console.log(grade.toFixed(1));
+        gradeEl.textContent = grade.toFixed(1);
     }
 }
